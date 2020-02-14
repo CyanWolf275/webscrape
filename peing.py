45b4969deabc71b5aadbc8c8110582d17d9b3bdd
jjdow75 <13918831764@163.com>
2020-02-14 21:46:04 +0800

    replace set with list

--- peing.py
+++ peing.py
@@ -2,22 +2,25 @@
 #This program extracts all the question links from the html file and fetches question and answer from them.
 #Then, it will output a csv file.
 
-import codecs, re, requests
-html_path = #html file of the extended peing page
+import codecs, re, requests, pyodbc
+html_path = "C:\\Users\\13918\\Desktop\\匿名で聞けちゃう！ねろさんの質問箱です _ Peing -質問箱-.html"#html file of the extended peing page
 f = codecs.open(html_path, "r", "utf-8")
 txt = f.read()
 filt = re.compile(r'\/ja\/q\/(.*?)\"')
 q_pattern = re.compile(r'<title>(.*?)\|')
 a_pattern = re.compile(r'<meta name=\"description\" content=\"(.*?)\">')
 
-lst = set(filt.findall(txt))
+lst = []
+for item in filt.findall(txt):
+    if item not in lst:
+        lst.append(item)
 f.close()
 with open("url.txt", "w") as file:
     for i in lst:
         file.write(i + " ")
 length = str(len(lst))
 i = 1
-with codecs.open("qa.csv", "w", "utf-8") as f2:
+with codecs.open("qa2.csv", "w", "utf-8") as f2:
     for url in lst:
         try:
             html = requests.get("https://peing.net/ja/q/" + url).text
--- peing_webpage.py
+++ peing_webpage.py
@@ -5,12 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 driver = webdriver.Chrome()
-driver.get("https://peing.net/ja/")#Complete this with the user's name
+driver.get("https://peing.net/ja/nero0x0")#Complete this with the user's name
 j = 0
-for i in range(6, 7 + 100, 5):#The number 100 can be adjusted according to the number of the questions
+for i in range(6, 7 + 500, 5):#The number 100 can be adjusted according to the number of the questions
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div[1]/div/div[7]/div/div/div/div[" + str(i) + "]/div")))
-    driver.execute_script("arguments[0].click();", driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[7]/div/div/div/div[" + str(i) + "]/div"))
+    wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div[1]/div/div[6]/div/div/div/div[" + str(i) + "]/div")))
+    driver.execute_script("arguments[0].click();", driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[6]/div/div/div/div[" + str(i) + "]/div"))
     print(j)
     j += 1
 input()
